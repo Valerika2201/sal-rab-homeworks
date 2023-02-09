5f9d20ab396feb209b676e45e66c2a28ba7c4b7d
Valeriya Karelova <v.karelova@brl.ru>
2023-02-09 14:32:50 +0300

     первое задание выполнено

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,14 +5,24 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message;
     // присвойте переменной message значение переменной productName
+    message = productName;
     // добавьте в конец переменной message пробел
+    message = massage + " ";
     // добавьте в конец переменной message текст 'за'
+    message = massage + "за";
     // добавьте в конец переменной message пробел
+    message = massage + " ";
     // добавьте в конец переменной message значение переменной productPrice
+    message = massage + let productPrice;
     // добавьте в конец переменной message пробел
+    message = massage + " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = massage + "теперь в корзине";
     // выведите переменную message в консоль
+    console.log(message);
+    
 // пример
     // Конец решения задания №1.1.
 

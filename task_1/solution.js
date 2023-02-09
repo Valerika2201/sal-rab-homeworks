ef7ee0c974bbae19ca4c90d2eca071b6a581686e
Valeriya Karelova <v.karelova@brl.ru>
2023-02-09 15:05:39 +0300

     задания выполнены идеально

--- task_1/solution.js
+++ task_1/solution.js
@@ -33,15 +33,12 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
     let newSum;
     newSum = (sum + delta);
     let newSumText;
     newSumText = (newSum + "₽");
     newSumText = `${newSum} ₽`;
-    
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+
 
     // Конец решения задания №1.3.
 
